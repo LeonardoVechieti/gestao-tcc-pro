1f2d45de6b90c9983b38b52ca6c4f1170a5df091
Leonardo Vechieti <leonardovechieti@outlook.com>
2026-07-04 18:27:27 -0300

    CONFIG | Front link

--- frontend/src/shared/api/api-client.ts
+++ frontend/src/shared/api/api-client.ts
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 export const apiClient = axios.create({
-  baseURL: import.meta.env.VITE_API_URL ?? 'http://localhost:3333',
+  baseURL: import.meta.env.VITE_API_URL ?? 'http://localhost:3003',
   headers: {
     Accept: 'application/json',
   },
